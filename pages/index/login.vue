50caeb55cc2d1b3d93273bb5d59a7e6e980fa8ff
JoshCunningHum <jkdimpas@gmail.com>
2023-11-07 13:50:29 +0800

    Rebase

--- pages/index/login.vue
+++ pages/index/login.vue
@@ -2,10 +2,7 @@
     <UContainer>
         <UForm 
             ref="form"
-            class="py-5 flex flex-col gap-4"
-            :schema="schema"
-            :state="values"
-            @submit="submit">
+            class="py-5 flex flex-col gap-4">
             <div class="text-3xl pb-2">
                 Login
             </div>
@@ -13,26 +10,21 @@
             <UFormGroup
                 label="Email"
                 name="email">
-                <UInput v-model="values.email" />
+                <UInput />
             </UFormGroup>
 
             <UFormGroup
                 label="Password"
                 name="password">
-                <UInput
-                    v-model="values.password"
-                    :type="`${isShowingPassword ? 'text' : 'password'}`"
-                    :ui="{ icon: { trailing: { pointer: '' } } }">
+                <UInput>
 
                     <template #trailing>
                         <UButton 
                         :padded="false"
                         variant="link"
-                        size="xl"
-                        @click="isShowingPassword = !isShowingPassword">
+                        size="xl">
                             <template #leading>
-                                <UIcon v-if="isShowingPassword" name="i-mdi-eye" />
-                                <UIcon v-else name="i-mdi-eye-off"/>
+                                <!-- TODO: Show password icons -->
                             </template>
                         </UButton>
                     </template>
@@ -58,48 +50,6 @@
 </template>
 
 <script setup>
-
-import { object, string} from 'yup';
-
-const isShowingPassword = ref(false);
-
-const form = ref(null);
-const values = ref({
-    email: undefined,
-    password: undefined
-})
-
-const schema = object({
-    email: string()
-        .email('Invalid email')
-        .required('This item is required')
-        .max(50, 'Max character limit')
-        .test('logincred', 'Invalid Login Credentials', () => {
-            return new Promise((resolve, reject) => resolve(!isCredentialsValid.value));
-        }),
-    password: string()
-        .min(8, 'Must be at least 8 characters')
-        .max(50, 'Max character limit')
-        .required('Required').test('logincred', 'Invalid Login Credentials', () => {
-        return new Promise((resolve, reject) => resolve(!isCredentialsValid.value));
-    }),
-});
-
-const submit = async(event) => {
-    const { email, password } = values.value;
-    const response = await useSignIn(email, password);
-    if(response !== true) {
-        isCredentialsValid.value = true;
-        form.value.validate();
-    }
-}
-
-// TODO: Implement multipla async login validation 
-const isCredentialsValid = ref(false);
-
-watch(values, () => {
-    isCredentialsValid.value = false;
-}, { immediate: true, deep: true })
 
 </script>
 
--- pages/index/register.vue
+++ pages/index/register.vue
@@ -2,10 +2,7 @@
     <UContainer :ui="{ padding: 'p-0' }">
         <UForm 
             ref="form"
-            class="py-5 flex flex-col gap-4"
-            :schema="schema"
-            :state="values"
-            @submit="submit">
+            class="py-5 flex flex-col gap-4">
             <div class="text-3xl pb-2">
                 Registration
             </div>
@@ -13,33 +10,25 @@
             <UFormGroup
                 label="Institution Name"
                 name="username">
-                <UInput v-model="values.username" />
+                <UInput/>
             </UFormGroup>
 
             <UFormGroup
                 label="Email"
                 name="email">
-                <UInput v-model="values.email" />
+                <UInput />
             </UFormGroup>
 
             <UFormGroup
                 label="Password"
                 name="password">
-                <UInput
-                    v-model="values.password"
-                    :type="`${isShowingPassword ? 'text' : 'password'}`"
-                    :ui="{ icon: { trailing: { pointer: '' } } }">
+                <UInput>
 
                     <template #trailing>
                         <UButton 
                         :padded="false"
                         variant="link"
-                        size="xl"
-                        @click="isShowingPassword = !isShowingPassword">
-                            <template #leading>
-                                <UIcon v-if="isShowingPassword" name="i-mdi-eye-off" />
-                                <UIcon v-else name="i-mdi-eye"/>
-                            </template>
+                        size="xl">
                         </UButton>
                     </template>
 
@@ -49,21 +38,13 @@
             <UFormGroup
                 label="Confirm Password"
                 name="confirmpassword">
-                <UInput
-                    v-model="values.confirmpassword"
-                    :type="`${isShowingPassword ? 'text' : 'password'}`"
-                    :ui="{ icon: { trailing: { pointer: '' } } }">
+                <UInput>
 
                     <template #trailing>
                         <UButton 
                         :padded="false"
                         variant="link"
-                        size="xl"
-                        @click="isShowingPassword = !isShowingPassword">
-                            <template #leading>
-                                <UIcon v-if="isShowingPassword" name="i-mdi-eye-off" />
-                                <UIcon v-else name="i-mdi-eye"/>
-                            </template>
+                        size="xl">
                         </UButton>
                     </template>
 
@@ -88,42 +69,6 @@
 </template>
 
 <script setup>
-import * as Yup from 'yup';
-import { useSignUp } from '~/composables/useSignUp';
-
-const existingEmail = ref('');
-
-const values = ref({
-    username: undefined,
-    email: undefined,
-    password: undefined,
-    confirmpassword: undefined
-});
-
-const form = ref(null);
-
-const schema = Yup.object({
-    username: Yup.string().required('Required').max(50, 'Max character limit'),
-    email: Yup.string().email('Invalid Email').required('Required').max(50, 'Max character limit')
-        .test('checkDupEmail', 'Email already used!', () => { 
-            return new Promise((resolve, reject) => {
-                resolve(values.value.email !== existingEmail.value);
-            })
-         }),
-    password: Yup.string().min(8, 'A minimum of 8 characters').max(50, 'Max character limit').required('Required'),
-    confirmpassword: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match').max(50, 'Max character limit')
-});
-
-const isShowingPassword = ref(false);
-
-const submit = async(event) => {
-    const { email, password, username } = values.value;
-    const response = await useSignUp(email, password, username);
-    if(response instanceof Error){
-        existingEmail.value = email;
-        form.value.validate();
-    }
-}
 
 </script>
 
