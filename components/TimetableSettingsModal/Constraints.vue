cc9b159b0669b8d758b40a241d7fa1cb1f0b5a94
JoshCunningHum <jkdimpas@gmail.com>
2024-01-07 16:16:14 +0800

    set to max button in constraints

--- components/TimetableSettingsModal/Constraints.vue
+++ components/TimetableSettingsModal/Constraints.vue
@@ -5,19 +5,19 @@ const settings = computed(() => timetableStore.selected?.data?.params.settings);
 
 const mim = computed({
   get: () => settings.value?.max_instructor_minutes_per_day || 0,
-  set: (v : number) => (settings.value === undefined) || (settings.value.max_instructor_minutes_per_day = v)
+  set: (v: number) => (settings.value === undefined) || (settings.value.max_instructor_minutes_per_day = v)
 })
 const msm = computed({
   get: () => settings.value?.max_student_minutes_per_day || 0,
-  set: (v : number)  => (settings.value === undefined) || (settings.value.max_student_minutes_per_day = v)
+  set: (v: number) => (settings.value === undefined) || (settings.value.max_student_minutes_per_day = v)
 })
 const mrm = computed({
   get: () => settings.value?.max_room_minutes_per_day || 0,
-  set: (v : number)  => (settings.value === undefined) || (settings.value.max_room_minutes_per_day = v)
+  set: (v: number) => (settings.value === undefined) || (settings.value.max_room_minutes_per_day = v)
 })
 const mcm = computed({
   get: () => settings.value?.max_consecutive_minutes || 0,
-  set: (v : number)  => (settings.value === undefined) || (settings.value.max_consecutive_minutes = v)
+  set: (v: number) => (settings.value === undefined) || (settings.value.max_consecutive_minutes = v)
 })
 
 </script>
@@ -27,27 +27,37 @@ const mcm = computed({
 
     <UFormGroup label="Max Instructor Minutes per Day"
       help="The highest amount of time (in minutes) instructors are permitted to spend in classes each day.">
-      <UInput type="number"  class="w-[150px]" v-model="mim"/>
+      <div class="flex gap-2">
+        <UInput type="number" class="w-[150px]" v-model="mim" />
+        <UButton @click="mim = Number.MAX_SAFE_INTEGER">Set to max</UButton>
+      </div>
     </UFormGroup>
 
     <UFormGroup label="Max Students Minutes per Day"
       help="The highest amount of time (in minutes) students are permitted to spend in classes each day.">
-      <UInput type="number"  class="w-[150px]" v-model="msm"/>
+      <div class="flex gap-2">
+        <UInput type="number" class="w-[150px]" v-model="msm" />
+        <UButton @click="msm = Number.MAX_SAFE_INTEGER">Set to max</UButton>
+      </div>
     </UFormGroup>
 
     <UFormGroup label="Max Room Minutes per Day"
       help="The highest amount of time (in minutes) rooms can be used for classes each day.">
-      <UInput type="number"  class="w-[150px]" v-model="mrm"/>
+      <div class="flex gap-2">
+        <UInput type="number" class="w-[150px]" v-model="mrm" />
+        <UButton @click="mrm = Number.MAX_SAFE_INTEGER">Set to max</UButton>
+      </div>
     </UFormGroup>
 
     <UFormGroup label="Max Consecutive Minutes"
       help="The highest amount of time (in minutes) that instructors may spend in class uninterrupted.">
-      <UInput type="number" class="w-[150px]" v-model="mcm" />
+      <div class="flex gap-2">
+        <UInput type="number" class="w-[150px]" v-model="mcm" />
+        <UButton @click="mcm = Number.MAX_SAFE_INTEGER">Set to max</UButton>
+      </div>
     </UFormGroup>
 
   </div>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
